b014232b47f1f31fc58d10d568e2e85b9b5c1c63
iQuerz <niki.tenis@gmail.com>
2022-04-19 13:19:34 +0200

    Add project to the repo

---
+++ PicSort/PicSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PicSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sort("", "");
+        }
+        static void sort(string dstPath, string srcPath)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write("\r{0}%   ", i);
+            }
+        }
+    }
+}
